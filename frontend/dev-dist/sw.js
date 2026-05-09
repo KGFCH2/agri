35c76e808d23ad3a265cdb08fad51eb1b0820944
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-05-09 06:46:54 +0000

    fixed errors on various page

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-8c5d9594'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.d683n9u2oi8"
+    "revision": "0.ma44dkcp49o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
