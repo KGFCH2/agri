e9f0b9252f369e386ddde8c67b8b9df9a043baa0
Deepika Gandla <155216833+deepikagandla7456@users.noreply.github.com>
2026-05-12 17:57:13 +0530

    Crop Disease Image Upload with AI Diagnosis (#583)
    Co-authored-by: Esha Jha <149310101+Eshajha19@users.noreply.github.com>

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-8c5d9594'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.d2i749c5sbk"
+    "revision": "0.murs4qemd4s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
